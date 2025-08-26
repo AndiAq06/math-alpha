7b70c26014c75ea74d69973d91d8572ca3b7f21e
ANDI ARDIANSYAH NASIR <222111890@stis.ac.id>
2025-08-26 08:25:45 +0700

    add file tiu

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
   closeBtn.addEventListener("click", toggleSidebar);
   overlay.addEventListener("click", toggleSidebar);
 });
+
+// Toggle pembahasan
+document.querySelectorAll(".btn-toggle").forEach((button) => {
+  button.addEventListener("click", () => {
+    const pembahasan = button.nextElementSibling;
+    if (pembahasan.style.display === "block") {
+      pembahasan.style.display = "none";
+      button.textContent = "Show Solution";
+    } else {
+      pembahasan.style.display = "block";
+      button.textContent = "Hide Solution";
+    }
+  });
+});
+
+// Toggle sidebar
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
